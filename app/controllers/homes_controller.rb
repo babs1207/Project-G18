dc219171f607eedfc8893cf84507bc05ea49308b
Brayder Berroteran <brayderb@gmail.com>
2019-03-04 23:24:27 -0300

    links to sign up

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,11 @@
+class HomesController < ApplicationController
+  
+  def index
+  end
+  
+  def registro
+  end
+
+  def sesion
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :drivers
-  devise_for :users
+  devise_for :drivers, path: 'drivers'
+  devise_for :users, path: 'users'
+
+  resources :homes, only: [:index]
+  get 'homes/registro', to: 'homes#registro', as: 'registro' 
+  get 'homes/sesion', to: 'homes#sesion', as: 'sesion' 
+  root 'homes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
