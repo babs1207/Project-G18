6a9b9da0db94fae85b2af56f59b438a786196daa
Brayder Berroteran <brayderb@gmail.com>
2019-03-05 15:12:22 -0300

    modified home/index

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,12 +1,4 @@
 class HomesController < ApplicationController
-  
-  def index
-  end
-  
-  def registro
-  end
 
-  def sesion
-  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :drivers, path: 'drivers'
-  devise_for :users, path: 'users'
+  devise_for :drivers, path: 'drivers', controllers: {
+    sessions: 'drivers/sessions',
+    registrations: 'drivers/registrations'
+  }
+
+  devise_for :users, path: 'users', controllers: {
+    sessions: 'users/sessions', 
+    registrations: 'users/registrations' 
+  }
 
   resources :homes, only: [:index]
+
   get 'homes/registro', to: 'homes#registro', as: 'registro' 
   get 'homes/sesion', to: 'homes#sesion', as: 'sesion' 
+
   root 'homes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
