c5d73d873f301e74cf416f83204cb5fc30dce915
Brayder Berroteran <brayderb@gmail.com>
2019-03-05 21:34:55 -0300

    change minor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery.min
-//= require jquery.dropotron.min
-//= require browser.min
-//= require breakpoints.min
-//= require util
-//= require main
-
+//= jquery.min
+//= jquery.dropotron.min
+//= jquery.scrollex.min
+//= browser.min
+//= breakpoints.min
+//= util
+//= main
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
